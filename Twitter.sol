8980bf2736f315e65109bce154beb96703a1ca1b
SheikhNoor <mdnurullah.co@gmail.com>
2025-03-04 09:41:03 +0530

    update

--- Twitter.sol
+++ Twitter.sol
@@ -2,10 +2,12 @@
 pragma solidity >=0.5.0 < 0.9.0;
 
 contract Twitter{
-    struct tweet{
+    struct Tweet{
         uint id;
         address author;
         string content;
+        uint createAt;
+
     }
 
     struct Message{
@@ -16,15 +18,69 @@ contract Twitter{
         uint createAt;
     }
 
-    mapping (uint => tweet) public tweets;
-    mapping (address => uint[]) public tweetsOff;
-    mapping (uint => Message[]) public conversation;
-    mapping (address=> mapping (address=>bool)) public  opetators;
-    mapping (address=>address[]) public followong;
+    mapping (uint => Tweet) public tweets;
+    mapping (address => uint[]) public tweetsOf;
+    mapping (address => Message[]) public conversation;
+    mapping (address=> mapping (address=>bool)) public  operators;
+    mapping (address=>address[]) public following;
 
     uint nextId;
-    uint nexxtMessage;
+    uint nextMessageId;
 
+    function _tweet(address _from, string memory _content) public {
+        tweets[nextId] = Tweet(nextId ,_from ,_content, block.timestamp);
+        nextId = nextId+1;
 
+    }
+
+    function _sendMessage(address _from,address _to, string memory _content) public{
+        conversation[_from].push(Message(nextMessageId, _content,_from,_to,block.timestamp));
+        nextMessageId++;
+    }
+
+    function tweet(string memory _content) public {
+        _tweet(msg.sender, _content);
+    }
+
+    function tweet(address _from,string memory _content) public{
+        _tweet(_from,_content);
+    }
+
+    function sendMessage(string memory _content,address _to) public {
+        _sendMessage(msg.sender,  _to ,   _content ); 
+    }
+
+    function sendMessage(address _from,address _to, string memory _content) public {
+        _sendMessage(_from, _to, _content);
+    }
+        
+    function follow(address _followed) public{
+        following[msg.sender].push(_followed);
+    }
+
+    // function unfollow(address _followed) public{
+    //     following[msg.sender].remove(_followed);
+    // }
+
+    function allow(address _operator) public {
+        operators[msg.sender][_operator] = true;
+    }
+
+    function disallow(address _operator) public{
+        operators[msg.sender][_operator] = false;
+    }
+
+    function getLatestTweets(uint count) public returns(Tweet[] memory){
+        require(count>0 && count<=nextId, "Count id not proper");
+        Tweet[] memory _tweets = new Tweet[] (count);
+        uint j;
+        for (uint i = nextId-count;i<=nextId;nextId++){
+            Tweet storage _structure = tweets[i];
+            _tweets[j] =Tweet( _structure.id, _structure.author,_structure.content,_structure.createAt);
+            j++;
+        }
+
+        return _tweets;
+    }
 
 }
